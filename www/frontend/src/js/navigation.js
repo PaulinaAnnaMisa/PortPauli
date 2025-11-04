5d8fe0f4b79415518a98c61a81ca54e90a9fbda8
Paulina Anna Misa <220901995+PaulinaAnnaMisa@users.noreply.github.com>
2025-11-04 15:03:38 +0100

    navbar update

---
+++ www/frontend/src/js/navigation.js
@@ -0,0 +1,33 @@
+let dropdown = document.getElementById("dropdown");
+let burgerBtn = document.getElementById("burgerBtn");
+
+burgerBtn.addEventListener("click", () => {
+  dropdown.classList.toggle("hidden"); // Menü ein-/ausblenden
+  let expanded = burgerBtn.getAttribute("aria-expanded") === "true";
+  burgerBtn.setAttribute("aria-expanded", !expanded);
+});
+
+// options with default values
+let options = {
+    placement: "bottom",
+    triggerType: "click",
+    offsetSkidding: 0,
+    offsetDistance: 10,
+    delay: 300,
+    ignoreClickOutsideClass: false,
+    onHide: () => {
+        console.log("dropdown has been hidden");
+    },
+    onShow: () => {
+        console.log("dropdown has been shown");
+    },
+    onToggle: () => {
+        console.log("dropdown has been toggled");
+    },
+};
+
+// instance options object
+let instanceOptions = {
+  id: "dropdown",
+  override: true
+};
